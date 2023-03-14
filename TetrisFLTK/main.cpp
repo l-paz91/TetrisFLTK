db84becc091590700b37cf69794d935f08f3be6e
longPointer <l-paz@hotmail.co.uk>
2023-03-14 20:36:05 +0000

    added dev diary comment

--- TetrisFLTK/main.cpp
+++ TetrisFLTK/main.cpp
@@ -4,6 +4,19 @@
 /*
 	Simple Tetris Game using C++ & FLTK
 
+	Based off "Code-It-Yourself! Tetris"
+
+	Following along with the video by javidx9 here:
+	https://www.youtube.com/watch?v=8OK8_tHeCIA&t=4s&ab_channel=javidx9
+
+	His original code is here:
+	https://github.com/OneLoneCoder/Javidx9/blob/master/SimplyCode/OneLoneCoder_Tetris.cpp
+
+	I've modified it quite a bit
+
+	Read my dev diary here:
+	https://lptcp.blogspot.com/2023/03/dev-diary-adventures-with-tetris-c.html
+
 */
 // https://github.com/l-paz91/
 // -----------------------------------------------------------------------------
